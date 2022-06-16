5c31e135832d8373aa83561ea47fdd62c4ea8348
Jordi <jordi.hoock@gmail.com>
2022-06-16 11:54:32 +0200

    Lower logging level

--- src/apps/cronjobs/settings.py
+++ src/apps/cronjobs/settings.py
@@ -2,3 +2,4 @@ import logging
 
 logging.getLogger('schedule').setLevel(level=logging.ERROR)
 logging.getLogger('root').setLevel(level=logging.ERROR)
+logging.getLogger('cronjobs_app').setLevel(level=logging.INFO)
