f40d5b733dbba7db0d8e8d932eaec62b5bbe6a63
José Ruiz <joseruiz@keemail.me>
2022-10-14 18:28:52 +0200

    SPR-736 Change endpoint name

--- src/apps/http/urls.py
+++ src/apps/http/urls.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from controllers.http import HttpController
 from core.authenticate import JwtResponse
 from core.buildable_items import BuildableResponse
@@ -183,5 +182,5 @@ async def register_fastapi_routes(http_controller: HttpController) -> list:
             methods=["get"],
         ),
         dict(path=r"/health", endpoint=http_controller.health, methods=["get"]),
-        dict(path=r"/medium_feed", endpoint=http_controller.medium_feed, methods=["get"])
+        dict(path=r"/feed", endpoint=http_controller.medium_feed, methods=["get"])
     ]
