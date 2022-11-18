d54bd8cfb821b05f1e7d986f647ba877d6c6e4c8
José Ruiz <joseruiz@keemail.me>
2022-11-18 18:34:42 +0100

    SPR-920 Set investigation laboratory values

--- src/core/shared/static/game_data/InstallationData.py
+++ src/core/shared/static/game_data/InstallationData.py
@@ -33,36 +33,37 @@ class InstallationData(GameData):
             None,
             "Investigation laboratory is needed in order to conduct new research",
             {
+                # level=1, cost_metal=150, cost_petrol=50, cost_crystal=600, energy_usage=0, production=0, capacity=0, time=1200, health=0, attack=0, experience=15, requirements=[], has_discount=0
                 0: BuildableItemLevelInfo(),
                 1: BuildableItemLevelInfo(
-                    1, 150, 50, 600, 0, 0, 0, 1200, 0, 0, 15, [], 0
+                    lvl=1, experience=293, health=2000, time=900, cost_metal=400, cost_crystal=800, cost_petrol=400, energy_usage=0, production=0, capacity=0, attack=0, requirements=[], has_discount=0
                 ),
                 2: BuildableItemLevelInfo(
-                    2, 200, 100, 800, 0, 0, 0, 1500, 0, 0, 25, [], 0
+                    lvl=2, experience=414, health=2000, time=1477, cost_metal=565, cost_crystal=1130, cost_petrol=565, energy_usage=0, production=0, capacity=0, attack=0, requirements=[], has_discount=0
                 ),
                 3: BuildableItemLevelInfo(
-                    3, 250, 200, 1000, 0, 0, 0, 1800, 0, 0, 35, [], 0
+                    lvl=3, experience=571, health=2197, time=2331, cost_metal=779, cost_crystal=1557, cost_petrol=779, energy_usage=0, production=0, capacity=0, attack=0, requirements=[], has_discount=0
                 ),
                 4: BuildableItemLevelInfo(
-                    4, 350, 300, 1200, 0, 0, 0, 2000, 0, 0, 45, [], 0
+                    lvl=4, experience=767, health=2350, time=3531, cost_metal=1046, cost_crystal=2092, cost_petrol=1046, energy_usage=0, production=0, capacity=0, attack=0, requirements=[], has_discount=0
                 ),
                 5: BuildableItemLevelInfo(
-                    5, 350, 400, 1400, 0, 0, 0, 2300, 0, 0, 55, [], 0
+                    lvl=5, experience=1004, health=2551, time=5126, cost_metal=1370, cost_crystal=2740, cost_petrol=1370, energy_usage=0, production=0, capacity=0, attack=0, requirements=[], has_discount=0
                 ),
                 6: BuildableItemLevelInfo(
-                    6, 450, 500, 1600, 0, 0, 0, 2600, 0, 0, 65, [], 0
+                    lvl=6, experience=1280, health=2807, time=7115, cost_metal=1746, cost_crystal=3493, cost_petrol=1746, energy_usage=0, production=0, capacity=0, attack=0, requirements=[], has_discount=0
                 ),
                 7: BuildableItemLevelInfo(
-                    7, 500, 800, 1800, 0, 0, 0, 2800, 0, 0, 85, [], 0
+                    lvl=7, experience=1588, health=3125, time=9426, cost_metal=2166, cost_crystal=4333, cost_petrol=2166, energy_usage=0, production=0, capacity=0, attack=0, requirements=[], has_discount=0
                 ),
                 8: BuildableItemLevelInfo(
-                    8, 650, 900, 2000, 0, 0, 0, 3000, 0, 0, 150, [], 0
+                    lvl=8, experience=1915, health=3508, time=11890, cost_metal=2613, cost_crystal=5225, cost_petrol=2613, energy_usage=0, production=0, capacity=0, attack=0, requirements=[], has_discount=0
                 ),
                 9: BuildableItemLevelInfo(
-                    9, 750, 1000, 3000, 0, 0, 0, 3400, 0, 0, 200, [], 0
+                    lvl=9, experience=3453, health=4198, time=14946, cost_metal=3140, cost_crystal=6281, cost_petrol=3140, energy_usage=0, production=0, capacity=0, attack=0, requirements=[], has_discount=0
                 ),
                 10: BuildableItemLevelInfo(
-                    10, 900, 1100, 4000, 0, 0, 0, 4000, 0, 0, 300, [], 0
+                    lvl=10, experience=4137, health=5026, time=18719, cost_metal=3762, cost_crystal=7524, cost_petrol=3762, energy_usage=0, production=0, capacity=0, attack=0, requirements=[], has_discount=0
                 ),
             },
         ),
